197c68dbbd0be28c09102d1cfe443e189770b333
Maksim Pukshlis <maksimpusklis7@gmail.com>
2023-10-02 16:56:43 +0300

    Added code comments for documentation

--- src/yandex_market/parser.py
+++ src/yandex_market/parser.py
@@ -10,12 +10,26 @@ from src.yandex_market.config_product import BREAD_CRUMBS, MAX_CONCURRENT_REQUES
 
 
 async def fetch_data(session, url, **kwargs):
+    """
+    Выполняет HTTP-запрос и возвращает JSON-ответ.
+
+    :param session: aiohttp.ClientSession
+    :param url: URL-адрес для запроса
+    :param kwargs: Дополнительные аргументы для запроса
+    :return: JSON-ответ
+        """
     async with session.post(url, **kwargs) as response:
         response_json = await response.json()
         return response_json
 
 
 async def save_data_to_csv(filename, data):
+    """
+    Сохраняет данные в CSV-файл.
+
+    :param filename: Имя CSV-файла
+    :param data: Данные для сохранения
+    """
     with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         for row in data:
@@ -23,6 +37,12 @@ async def save_data_to_csv(filename, data):
 
 
 async def extract_product_links(response_json):
+    """
+    Извлекает ссылки на продукты из JSON-ответа.
+
+    :param response_json: JSON-ответ
+    :return: Словари ссылок на продукты
+    """
     product_links = {}
     product_show_place = response_json['collections']['productShowPlace'].values()
 
@@ -46,10 +66,14 @@ async def extract_product_links(response_json):
 
 
 async def extract_prices_titles_is_resales(response_json):
+    """
+    Извлекает цены, названия, флаги перепродажи и спецификации перепродажи из JSON-ответа.
+
+    :param response_json: JSON-ответ
+    :return: Словари цен, названий, флагов перепродажи и спецификаций перепродажи
+    """
     prices = {}
-
     titles = {}
-
     resales = {}
     resales_specs = {}
 
@@ -75,6 +99,16 @@ async def extract_prices_titles_is_resales(response_json):
 
 
 async def get_product_link(product, products_links, product_links_offer, resales, resales_specs):
+    """
+    Получает ссылку на продукт с параметрами перепродажи (если есть).
+
+    :param product: Данные о продукте
+    :param products_links: Словарь ссылок на продукты
+    :param product_links_offer: Словарь ссылок на продукты (предложения)
+    :param resales: Словарь флагов перепродажи
+    :param resales_specs: Словарь спецификаций перепродажи
+    :return: Ссылка на продукт с параметрами
+    """
     product_id = product['id']
     link = product_links_offer.get(product['id'], products_links[product['id']]).replace(',', '')
 
@@ -88,6 +122,14 @@ async def get_product_link(product, products_links, product_links_offer, resales
 
 
 async def get_category(base_url, headers, params):
+    """
+    Получает данные о продуктах в категории.
+
+    :param base_url: Базовый URL для API категории
+    :param headers: Заголовки для HTTP-запроса
+    :param params: Параметры для запроса API
+    :return: Список данных о продуктах
+    """
     result_category = []
     async with aiohttp.ClientSession() as session:
         response_json = await fetch_data(session, base_url, headers=headers, json=params)
@@ -119,6 +161,12 @@ async def get_category(base_url, headers, params):
 #############################################################PRODUCT####################################################
 
 async def get_brand_name(title):
+    """
+    Извлекает имя бренда из названия продукта.
+
+    :param title: Название продукта
+    :return: Имя бренда
+    """
     brand_name = ''
 
     for item in title.split():
@@ -130,6 +178,12 @@ async def get_brand_name(title):
 
 
 async def get_stock(response_json):
+    """
+    Получает данные о доступном количестве товара на складе.
+
+    :param response_json: JSON-ответ
+    :return: Доступное количество товара на складе
+    """
     try:
         stock = \
             response_json['scaffold']['bottomView']['divData']['states'][0]['div']['bottomItemsRef'][0]['custom_props'][
@@ -140,6 +194,12 @@ async def get_stock(response_json):
 
 
 async def get_price(response_json):
+    """
+    Получает цену продукта.
+
+    :param response_json: JSON-ответ
+    :return: Цена продукта
+    """
     try:
         price = response_json['scaffold']['wishButtonParams']['price'].get('value', '')
         return price
@@ -148,6 +208,12 @@ async def get_price(response_json):
 
 
 async def get_params_for_request(url):
+    """
+    Извлекает параметры для выполнения запроса на основе URL.
+
+    :param url: URL-адрес продукта
+    :return: Параметры запроса
+    """
     parsed_url = urlparse(url)
 
     query_parameters = parse_qs(parsed_url.query)
@@ -166,6 +232,12 @@ async def get_params_for_request(url):
 
 
 async def get_rating_and_reviews_count(response_json):
+    """
+    Получает рейтинг и количество отзывов о продукте.
+
+    :param response_json: JSON-ответ
+    :return: Рейтинг и количество отзывов о продукте
+    """
     rating = 0
     reviews_count = 0
 
@@ -177,7 +249,30 @@ async def get_rating_and_reviews_count(response_json):
     return rating, reviews_count
 
 
+async def get_title(response_json):
+    """
+    Получает название продукта из JSON-ответа.
+
+    :param response_json: JSON-ответ
+    :return: Название продукта
+    """
+    try:
+        title = response_json['scaffold'].get('title')
+        return title
+    except KeyError:
+        return ''
+
+
 async def get_products(products, base_url, headers, json_data):
+    """
+    Получает данные о продуктах и обрабатывает их.
+
+    :param products: Список продуктов для обработки
+    :param base_url: Базовый URL для API
+    :param headers: Заголовки для HTTP-запросов
+    :param json_data: Данные JSON для запросов
+    :return: Список обработанных данных о продуктах
+    """
     result_products = []
     semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
 
@@ -196,15 +291,19 @@ async def get_products(products, base_url, headers, json_data):
     return result_products
 
 
-async def get_title(response_json):
-    try:
-        title = response_json['scaffold'].get('title')
-        return title
-    except KeyError:
-        return ''
-
-
 async def fetch_and_process_product(session, base_url, product, params, headers, json_data, result_products, semaphore):
+    """
+    Выполняет запрос на продукт и обрабатывает полученные данные.
+
+    :param session: aiohttp.ClientSession
+    :param base_url: Базовый URL для API
+    :param product: Данные о продукте
+    :param params: Параметры для запроса
+    :param headers: Заголовки для HTTP-запроса
+    :param json_data: Данные JSON для запроса
+    :param result_products: Список для сохранения обработанных данных о продуктах
+    :param semaphore: Семафор для ограничения параллельных запросов
+    """
     async with semaphore:
         response_json = await fetch_data(session, base_url, params=params, headers=headers, json=json_data)
 
